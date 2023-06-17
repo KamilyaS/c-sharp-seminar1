0ad6fe427d8a8fd0ff2790edb7892694654edf2f
Kamilya <you@example.com>
2023-06-17 21:37:08 +0300

    add Task 5

---
+++ task 5/Program.cs
@@ -0,0 +1,9 @@
+﻿/*Напишите программу которая на входе принимает трехзначное число, а на выходе выводит последнюю цифру числа
+456 ->6
+782 ->2
+918 ->8 */
+
+Console.WriteLine ("Add number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+numberA = numberA %10;
+Console.Write(numberA);

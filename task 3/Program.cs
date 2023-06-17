65a17cea9ecd4cc2394306c5a4b8324d9451b9c6
Kamilya <you@example.com>
2023-06-17 21:01:43 +0300

    add task3

---
+++ task 3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу которая будет выдавать день недели по заданному номеру
+
+Console.WriteLine("Add number day of week: ");
+
+int DayWeek = Convert.ToInt32(Console.ReadLine());
+
+if (DayWeek == 1) 
+    Console.WriteLine("Monday");
+else if (DayWeek == 2) 
+    Console.WriteLine("Tuesday");
+else if (DayWeek == 3) 
+    Console.WriteLine("Wednesday");
+else if (DayWeek == 4) 
+    Console.WriteLine("Thursday");
+else if (DayWeek == 5) 
+    Console.WriteLine("Friday");
+else if (DayWeek == 6) 
+    Console.WriteLine("Saturday");
+else if (DayWeek == 7) 
+    Console.WriteLine("Sunday");
+else Console.WriteLine("False");

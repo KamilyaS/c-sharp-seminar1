ce7a9294bd7e8ccc8550d50b38813dbb50283960
Kamilya <you@example.com>
2023-06-17 20:04:26 +0300

    Add task2

--- task 1/Program.cs
+++ task 1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Add number"); //Вывод на терминал приглашение
+﻿// Напишите программу, которая на вход принимает число и выводит его квадрат
+Console.Write("Add number"); //Вывод на терминал приглашение
 int number = Convert.ToInt32(Console.ReadLine()); // Чтение целого числа из терминала
 int result = number * number;
 Console.WriteLine("Square added number:"  + result);

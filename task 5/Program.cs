718caabcdbe4e144d11d5e946de38ce9f2497c13
Kamilya <you@example.com>
2023-06-17 21:54:42 +0300

    correct task 5

--- task 5/Program.cs
+++ task 5/Program.cs
@@ -3,7 +3,15 @@
 782 ->2
 918 ->8 */
 
-Console.WriteLine ("Add number: ");
-int numberA = Convert.ToInt32(Console.ReadLine());
-numberA = numberA %10;
-Console.Write(numberA);
+    Console.WriteLine ("Add number: ");
+    int numberA = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+if  (numberA >99 && numberA < 1000)
+{ 
+    numberA = numberA %10;
+    Console.WriteLine("Last number" + numberA);
+}
+else 
+{
+    Console.WriteLine("Add number from 1 to 999");
+}
+

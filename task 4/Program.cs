acb71fad6fc13ca9f041ad7fa18d76c928fa973c
Kamilya <you@example.com>
2023-06-17 21:23:54 +0300

    add Task 4

---
+++ task 4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Нужно написать программу, которая на входе принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N 
+
+Console.WriteLine("Add number: ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+int numberB = 0;
+if (numberA >0) {numberB = -numberA;}
+else {
+    numberB = numberA;
+    numberA = -numberA;
+}
+while (numberA >= numberB)
+{
+    Console.Write(numberB + " ");
+    numberB++;
+}

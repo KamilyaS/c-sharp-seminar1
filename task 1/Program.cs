43ad616c88ece49454168a753999f8eb413ef330
Kamilya <you@example.com>
2023-06-17 19:33:54 +0300

    add task

---
+++ task 1/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Add number"); //Вывод на терминал приглашение
+int number = Convert.ToInt32(Console.ReadLine()); // Чтение целого числа из терминала
+int result = number * number;
+Console.WriteLine("Square added number:"  + result);
+System.Console.WriteLine($"Square added number: {result} write new text");
+

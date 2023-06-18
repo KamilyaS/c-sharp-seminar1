42d07500b7a7f3d0769833d977a5b8c8aca66a7e
Kamilya <you@example.com>
2023-06-18 19:18:44 +0300

    add task 8

---
+++ task 8/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Напишите программу, которая будет принимать на вход два числа и выводить, 
+является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит
+остаток от деления
+34, 5 -> не кратно, остаток 4
+16, 4 -> кратно*/
+
+Console.WriteLine("Add number 1");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Add number 2");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+if(numberA % numberB == 0)
+{
+    Console.WriteLine("Numbers are multiples");
+}
+else 
+{
+    Console.WriteLine("Numbers are not multiples. Remainder of the division" + numberA % numberB); //Числа не кратные остаток от деления:..
+}

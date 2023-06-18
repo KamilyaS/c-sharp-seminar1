119c621775ccf703ca30e2bce8eb70b0b7d20c17
Kamilya <you@example.com>
2023-06-18 18:19:21 +0300

    add task 6

---
+++ task 6/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая выводит случайное числов из отрезка [10,99] и показывает наибольшую цифру числа
+78 ->8
+12 ->2
+85 ->8 */
+
+Random rand = new Random();
+int randomNumber = rand.Next(10, 100);
+//int randomNumber = new Random().Next(10, 100); //[10, 100) такая конструкция громоздская, при работе с массивом то эта функция будет занимать большее количество памяти
+
+Console.WriteLine(randomNumber);
+
+//int leftNumber = randomNumber / 10;
+//int rightNumber = randomNumber % 10;
+
+if (randomNumber / 10 > randomNumber % 10)
+{
+    Console.WriteLine($"leftNumber more than -> {randomNumber / 10}");
+}
+else if (randomNumber % 10 > randomNumber / 10)
+{
+    Console.WriteLine($"rightNumber more than -> {randomNumber % 10}");
+}
+else 
+{
+    Console.WriteLine("The numbers are equal");
+}

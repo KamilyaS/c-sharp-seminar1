2237ea6e286ec8fc635f2b88908b3347a1510228
Kamilya <you@example.com>
2023-06-18 19:35:01 +0300

    add task 9

---
+++ task 9/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно
+7 и 23
+14 -> no
+46 -> no
+161 -> yes */
+
+Console.WriteLine("Add number 1");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number % 7 == 0 && number % 23 == 0)
+{
+    Console.WriteLine($"{number} -> Yes");
+}
+else 
+{
+    Console.WriteLine($"{number} -> No");
+}

649cad03267a82c3dbc309f76772a6e9212202af
Kamilya <you@example.com>
2023-06-19 13:28:52 +0300

    add task 14

---
+++ task 14/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Напишите программу, которая на вход принимает число N
+и выдает таблицу квадратов чисел от 1 до N
+5 -> 1,4,9,16,25
+2 -> 1,4 */
+
+Console.WriteLine("Enter number");
+int number = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+
+while (i<= number)
+{
+    System.Console.WriteLine(Math.Pow(i, 2));
+    i++;
+}
